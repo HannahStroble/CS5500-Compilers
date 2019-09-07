ba388062f7c48dc762d9d5d9cf3dcec4313dc2db
hmrvg9 <hmrvg9>
2019-09-06 22:00:50 -0500

    draft of grammars

--- hw2/syntax_analyzer.py
+++ hw2/syntax_analyzer.py
@@ -94,6 +94,203 @@ def proc_dec():
     block()
 
 def proc_hdr():
+    # procedure
+    # ident
+    # ;
+
+def stmt_part():
+    compound()
+
+def compound():
+    # begin
+    stmt()
+    stmt_lst()
+    # end
+
+def stmt_lst():
+    # ;
+    stmt()
+    stmt_lst()
+    # else
+    # epsilon
+
+def stmt():
+    assign()
+    # if else
+    read()
+    # if else
+    write()
+    # if else
+    condition()
+    # if else
+    while()
+    # if else
+    compound()
+
+def assign():
+    variable()
+    # := 
+    expr()
+
+def proc_stmt():
+    proc_ident()
+
+def proc_ident():
+    # ident
+
+def read():
+    # read
+    # (
+    input_var()
+    input_lst()
+    # )
+
+def input_lst():
+    # ,
+    input_var()
+    input_lst()
+    # )
+
+def input_var():
+    variable()
+
+def write():
+    # write
+    # (
+    output()
+    output_lst()
+    # )
+
+def output_lst():
+    # ,
+    output()
+    output_lst()
+    # else
+    # epsilon
+
+def output():
+    expr()
+
+def condition():
+    # if
+    expr()
+    # then
+    stmt()
+    elsePart()
+
+def elsePart():
+    # else
+    stmt()
+    # do
+    stmt()
+
+def while():
+    # while
+    expr()
+    # do
+    stmt()
+
+def expr():
+    simple_expr()
+    opExpr()
+
+def opExpr():
+    rel_op()
+    simple_expr()
+    # else
+    # epsilon
+
+def simple_expr():
+    term()
+    add_op_lst()
+
+def add_op_lst():
+    add_op()
+    term()
+    add_op_lst
+    # else
+    # epsilon
+
+def term():
+    factor()
+    mult_op_lst()
+
+def mult_op_lst():
+    mult_op()
+    factor()
+    mult_op_lst()
+    # else
+    # epsilon
+
+def factor():
+    sign()
+    variable()
+    # if else
+    const()
+    # if else
+    # (
+    expr()
+    # )
+    # if else
+    # not
+    factor()
+
+def sign():
+    # + 
+    # else if 
+    # -
+    # else if
+    # epsilon
+
+def add_op():
+    # + 
+    # if else
+    # -
+    # if else
+    # or
+
+def mult_op():
+    # *
+    # if else
+    # div
+    # if else
+    # and
+
+def rel_op():
+    # <
+    # if else
+    # <+
+    # if else
+    # <>
+    # if else
+    # =
+    # if else
+    # >
+    # if else
+    # >=
+
+def variable():
+    # ident
+    idx_var()
+
+def idx_var():
+    # [
+    expr()
+    # ]
+    # else
+    # epsilon
+
+def const():
+    # intconst
+    # if else
+    # charconst
+    # if else
+    bool_const()
+
+def bool_const():
+    # true
+    # if else
+    # false
 
 
     
