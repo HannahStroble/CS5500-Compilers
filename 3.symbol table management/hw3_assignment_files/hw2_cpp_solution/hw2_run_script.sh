01724829a6a4d9c3542024f271945fae49873409
Hannah Reinbolt <hannah@master-logic.com>
2019-09-19 13:28:26 -0500

    set up initial auto check feature

--- 3.symbol table management/hw3_assignment_files/hw2_cpp_solution/hw2_run_script.sh
+++ 3.symbol table management/hw3_assignment_files/hw2_cpp_solution/hw2_run_script.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # To run:
-#	bash hw_run_script.sh
+#	bash hw2_run_script.sh
+#	OR
+#	./hw2_run_script.sh
 
 # This script assumes you have compiled your solution and called
 # the executable "parser", as shown:
 
-#	flex <filename>.l
-#	bison <filename>.y
-#	g++ <filename>.tab.c -o parser
+#	g++ -std=c++11 *.cpp -o driver
+#	chmod +x hw2_run_script.sh
+#	./hw2_run_script.sh
 
 # This script also assumes you have the provided sample input files from
 # Canvas in a directory called "sample_input", and the expected output files
-# in a directory called "expected_output". It also requires two
-# empty directories named "actual_output" and "reports".
+# in a directory called "sample_output". It also requires two
+# empty directories named "actual_output" and "final_output".
 
 # The script will produce a side-by-side comparison of your output
 # to the expected output. Any lines that have a "|", "<", or ">"
@@ -26,10 +28,10 @@
 inputs=`ls sample_input --ignore-backups`
 
 for i in $inputs; do
-    ./parser ./sample_input/$i \
+    ./driver ./sample_input/$i \
 	     > ./actual_output/$i.out
 
-    diff ./actual_output/$i.out ./expected_output/$i.out > \
-         ./reports/$i --ignore-space-change --ignore-case \
+    diff ./actual_output/$i.out ./sample_output/$i.out > \
+         ./final_output/$i --ignore-space-change --ignore-case \
          --side-by-side --ignore-blank-lines
 done
