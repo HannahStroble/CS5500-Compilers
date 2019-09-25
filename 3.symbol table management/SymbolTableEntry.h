adb03f0cb1ecd0ab8314c6c4a793cca0c5950e91
ackgg3 <ackgg3@mst.edu>
2019-09-24 22:32:59 -0500

    Did the magic!

--- 3.symbol table management/SymbolTableEntry.h
+++ 3.symbol table management/SymbolTableEntry.h
@@ -2,16 +2,27 @@
 #define SYMBOL_TABLE_ENTRY_H
 
 #include <string>
+#include "parser.h"
 using namespace std;
 
 #define UNDEFINED  -1
+
+struct array_parts
+{
+  int start;
+  int end;
+  int type;
+};
 
 class SYMBOL_TABLE_ENTRY
 {
 private:
   // Member variables
   string name;
+  bool isArray;
   int typeCode;
+  array_parts parts;
+
 
 public:
   // Constructors
@@ -21,11 +32,24 @@ public:
   {
     name = theName;
     typeCode = theType;
+    isArray = false;
   }
+  
+  SYMBOL_TABLE_ENTRY(const string theName, const array_parts array)
+  {
+    name = theName;
+    parts = array;
+    isArray = true;
+  }
+
+
+
 
   // Accessors
   string getName() const { return name; }
+  bool getArray() const { return isArray; }
   int getTypeCode() const { return typeCode; }
+  array_parts getParts() const { return parts; }
 };
 
 #endif  // SYMBOL_TABLE_ENTRY_H
